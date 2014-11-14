44d52cf6c0744711f6c469a7d0eb00d60501bae2
anonymousHomeworkHqc <theoctopushasnofriends@gmail.com>
2014-11-14 12:53:31 +0200

    Database Models

---
+++ OplachiSe/OplachiSe.Data/OplachiSeDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace OplachiSe.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using OplachiSe.Models;
+
+    public class OplachiSeDbContext : IdentityDbContext<User>
+    {
+        public OplachiSeDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static OplachiSeDbContext Create()
+        {
+            return new OplachiSeDbContext();
+        }
+    }
+}
---
+++ OplachiSe/OplachiSe.Models/Complain.cs
@@ -0,0 +1,54 @@
+﻿namespace OplachiSe.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Complain
+    {
+        private ICollection<Comment> comments;
+        private ICollection<Vote> votes;
+        private ICollection<Picture> pictures;
+
+        public Complain()
+        {
+            this.comments = new HashSet<Comment>();
+            this.votes = new HashSet<Vote>();
+            this.pictures = new HashSet<Picture>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(20)]
+        [MaxLength(300)]
+        public string Content { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public virtual User Author { get; set; }
+
+        public virtual ICollection<Picture> Pictures
+        {
+            get { return this.pictures; }
+            set { this.pictures = value; }
+        }
+
+        public virtual ICollection<Comment> Comments
+        {
+            get { return this.comments; } 
+            set { this.comments = value; }
+        }
+
+        public virtual ICollection<Vote> Votes
+        {
+            get { return this.votes; }
+            set { this.votes = value; }
+        }
+    }
+}
