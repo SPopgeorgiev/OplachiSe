e018cca752b99a160829b45fb938235563190405
anonymousHomeworkHqc <theoctopushasnofriends@gmail.com>
2014-11-14 17:31:36 +0200

    Installed kendo

--- OplachiSe/OplachiSe.Web/App_Start/BundleConfig.cs
+++ OplachiSe/OplachiSe.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,22 @@ namespace OplachiSe.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+            RegisterScriptBundles(bundles);
+            RegisterStylesBundles(bundles);
+
+            BundleTable.EnableOptimizations = false;
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/kendo.all.min.js",
+                "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/kendo/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,6 +36,14 @@ namespace OplachiSe.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+        }
+
+        private static void RegisterStylesBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                "~/Content/kendo/kendo.common.min.css",
+                "~/Content/kendo/kendo.common-bootstrap.core.min.css",
+                "~/Content/kendo/kendo.blueopal.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
